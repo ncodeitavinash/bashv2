50a7316ee802a59672839511d150004944f31b05
ncodeitavinash <avinash.g.s@ncodeit.com>
2018-05-14 20:37:24 +0530

    get all array and print it

---
+++ getallprint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+arr=(aa bb cc dd ee ff gg)
+
+echo ${arr[*]} # all array
+
+echo ${arr[@]:0}   # aa bb cc dd ee ff gg
+
+echo ${arr[@]:1}   # bb cc dd ee ff gg
+
+echo ${arr[@]:2:3} # cc dd ee
+
+for i in ${arr[*]}
+do
+	echo $i
+done
+
