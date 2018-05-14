fccc07e6a4f1306e8c0c3a30e1037318af7580df
ncodeitavinash <avinash.g.s@ncodeit.com>
2018-05-14 20:39:27 +0530

    Adding element to array

---
+++ addingelement.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+arr=(aa bb cc dd ee ff gg)
+
+echo ${arr[*]}
+
+arr=( "${arr[@]}" "newElem" ) # from right
+
+echo ${arr[*]}
+
+arr=( "newElem" "${arr[@]}" ) # from left
+
+echo ${arr[*]}
