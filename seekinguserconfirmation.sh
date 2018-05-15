6e6bef41ce548d5a5dee8adcf1e1f67449fa823c
ncodeitavinash <avinash.g.s@ncodeit.com>
2018-05-16 03:25:53 +0530

    seeking user confirmation

---
+++ seekinguserconfirmation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+seek_confirmation() {
+  printf "\n${bold}$@${reset}"
+  read -p " (y/n) " -n 1
+  printf "\n"
+}
+
+# Test whether the result of an 'ask' is a confirmation
+is_confirmed() {
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  return 0
+fi
+return 1
+}
