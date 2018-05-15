c5330d9334d7653f1b3c955ba79dc58a28ef4203
ncodeitavinash <avinash.g.s@ncodeit.com>
2018-05-16 00:56:37 +0530

    cat until Eof

---
+++ catuntileof.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cat <<-EOF >/root/avinash
+EOF
+#The - option to mark a here document limit string (<<-EOF) suppresses leading tabs (but not spaces) in the output. This may be useful in making a script more readable.
+
+#cat something to stdout by removing the redirection to file (>/root/avinash).
