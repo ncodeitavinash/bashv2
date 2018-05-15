bd28b23734bbdc927414f01764455334090b9f5c
ncodeitavinash <avinash.g.s@ncodeit.com>
2018-05-16 04:48:44 +0530

    Alias

---
+++ alias.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+alias df='df -h'
+alias du='du -h --max-depth=1'
+alias grep='grep --color'
+alias ll='ls -lh --color=auto'
+alias la='ls -lah --color=auto'
+ 
+alias m-install='mvn clean install'
+ 
+alias ga='git add'
+alias gc='git status -s -uno; git commit -a -m "Archiving"'
